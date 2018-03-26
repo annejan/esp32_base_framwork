9b680b4ac9ac815d38f2d94589127645931c9682
Markus Bechtold <markus@r-bechtold.de>
2018-03-26 21:33:49 +0200

    remove build dir

--- build/jsmn/src/jsmn.d
+++
@@ -1,5 +0,0 @@
-src//jsmn.o: /Users/markus/esp/esp-idf/components/jsmn/src//jsmn.c \
- /Users/markus/esp/esp-idf/components/jsmn/include/jsmn.h
-
-/Users/markus/esp/esp-idf/components/jsmn/include/jsmn.h:
-/Users/markus/esp/esp-idf/components/jsmn/src//jsmn.c:
--- build/wifi_manager/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/main/wifi_manager/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/wifi_manager -lwifi_manager
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += wifi_manager
-component-wifi_manager-build: 
